800c1cfe8d014e7fe43400ce2de88257a7167375
Surendra Chakravarthula <surendra.chakravarthula.24.1@mountblue.tech>
2023-05-22 02:24:29 +0530

    Add POJO class
    POJO class helps to send data in object form everytime from DAO.

---
+++ src/main/java/contact_us/ContactUsPOJO.java
@@ -0,0 +1,37 @@
+package contact_us;
+
+public class ContactUsPOJO {
+	private int id;
+    private String name;
+    private String email;
+    private String message;
+    
+	public ContactUsPOJO(int id, String name, String email, String message) {
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.message = message;
+	}
+
+	public ContactUsPOJO(String name, String email, String message) {
+		this.name = name;
+		this.email = email;
+		this.message = message;
+	}
+
+	public int getId() {
+		return id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+}
