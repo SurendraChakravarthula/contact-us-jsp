7b8d7eed77e88441db1bdd582f30c76865e9c97d
Surendra Chakravarthula <surendra.chakravarthula.24.1@mountblue.tech>
2023-05-22 17:30:04 +0530

    Feat Restore archive messages
    Restore option given for archived messages by clicked upon.It will be shown in active messages

---
+++ src/main/java/contact_us/RestoreToActiveMessage.java
@@ -0,0 +1,42 @@
+package contact_us;
+
+import java.util.List;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet(name="restoreMessage",urlPatterns="/restoreFromActive")
+public class RestoreToActiveMessage extends HttpServlet {
+
+	protected void service(HttpServletRequest request,HttpServletResponse response) {
+		doPost(request,response);
+	}
+	
+	protected void doPost(HttpServletRequest request,HttpServletResponse response) {
+		 PostgresqlDao adminLogin=new PostgresqlDao();
+		 
+		 try {
+		 int id=Integer.parseInt(request.getParameter("id"));
+		 boolean restoreMessage=adminLogin.restoreMessage(id);
+		 
+		 if(restoreMessage) {
+			 HttpSession session=request.getSession(false);
+			 
+			 List<ContactUsPOJO> activeMessages=adminLogin.getActiveMessages();
+			 session.setAttribute("activeMessages", activeMessages);
+			   
+			 List<ContactUsPOJO> archiveMessages=adminLogin.getArchiveMessages();
+			 session.setAttribute("archiveMessages", archiveMessages);
+			   
+			 response.sendRedirect("ContactUsMessages.jsp");
+		 } else {
+			 response.getWriter().println("<html><body><h1>Something Wrong! Please try Again</h1></body></html>");
+		 }
+		 }catch(Exception e) {
+			 e.printStackTrace();
+		 }
+	}		
+}
