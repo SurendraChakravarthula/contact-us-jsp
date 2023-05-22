d048b74115af109a22e58f06f40d9e498c3dcbdf
Surendra Chakravarthula <surendra.chakravarthula.24.1@mountblue.tech>
2023-05-22 14:53:05 +0530

    Feat logout
    Logout servlet added for logging out

---
+++ src/main/java/contact_us/LogOut.java
@@ -0,0 +1,27 @@
+package contact_us;
+
+import java.io.IOException;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet(name="logout",urlPatterns="/Logout")
+public class LogOut extends HttpServlet {
+   protected void service(HttpServletRequest request,HttpServletResponse response) {
+	   doPost(request,response);
+   }
+   
+   protected void doPost(HttpServletRequest request,HttpServletResponse response) {
+	   HttpSession session=request.getSession(false);
+	   session.setAttribute("username", null);
+	   session.invalidate();
+	   try {
+		response.sendRedirect("LoginPage.jsp");
+	} catch (IOException e) {
+		e.printStackTrace();
+	}
+   }
+}
